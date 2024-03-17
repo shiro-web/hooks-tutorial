609946836608bf2fd79ea53ea1b297e5b9484a22
shiro-web <shiro-kwsm-46@docomo.ne.jp>
2024-03-17 22:06:14 +0900

    first commit

--- src/lessons/Lesson1/Lesson1_1.tsx
+++ src/lessons/Lesson1/Lesson1_1.tsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+
 const Lesson1_1 = () => {
+  const [age,setAge] = useState<number>(0);
+
+  const handleClick = () => {
+    setAge((prev) => prev + 1);
+    setAge((prev) => prev + 1);
+    setAge((prev) => prev + 1);
+    console.log(age)
+  }
+  
   return (
     <div>
       <input type="text" />
-      <button className="border p-2 rounded-md bg-red-100">Add Age</button>
-      <p></p>
+      <button className="border p-2 rounded-md bg-red-100" onClick={handleClick}>Add Age</button>
+      <p>You are {age}</p>
     </div>
   );
 };
